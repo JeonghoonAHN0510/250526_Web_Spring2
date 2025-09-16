3c77296491c81641a7ca0aa8dc905799ae72721b
JeonghoonAHN0510 <jeonghoonahn0510@gmail.com>
2025-09-16 12:17:40 +0900

    study[React] : 컴포넌트 + JSX

--- src/main/reactapp/src/example/Day01/Component3.jsx
+++ src/main/reactapp/src/example/Day01/Component3.jsx
@@ -18,9 +18,34 @@ const Component3 = ( props ) => {
             <div>
                 { 10 + 13 }
             </div>
+            { /* 다른 컴포넌트 불러오기 + 자료 전달(props) */ }
+            <SubCom1 key1={name} key2="40"/>
         </> 
     ) // return end
 } // func end
 
 // [2] 컴포넌트 내보내기
 export default Component3;
+
+// [3] 컴포넌트 생성 - props 예제
+const SubCom1 = ( props ) => {
+
+    const obj = { name : "강호동", age : 50 };
+    console.log( obj );
+
+    // 1. props 확인
+    console.log( props );
+    // 2. props 구조 분해 -> 객체/배열 구조를 쪼개는 방식
+    const { key1, key2 } = props;
+    console.log( key1 );
+    console.log( key2 );
+
+    // <-------- JSX 문법 적용
+    return(
+        <>
+            <h4> { obj.name }님의 나이는 { obj.age }입니다. </h4>
+            <h4> { props.key1 }님의 나이는 { props.key2 }입니다. </h4>
+            <h4> { key1 }님의 나이는 { key2 }입니다. </h4>
+        </>
+    ) // return end
+} // func end

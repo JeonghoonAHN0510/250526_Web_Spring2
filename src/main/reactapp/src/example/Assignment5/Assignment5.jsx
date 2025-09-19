cf411a32b4ae5fc532720b0ed06dbefe183ded56
JeonghoonAHN0510 <jeonghoonahn0510@gmail.com>
2025-09-19 13:12:50 +0900

    Assignment[React] : 토론 등록/삭제/조회 프론트 구현

--- src/main/reactapp/src/example/Assignment5/Assignment5.jsx
+++ src/main/reactapp/src/example/Assignment5/Assignment5.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
-import './Assignment5.css'
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table, Button, Form, Modal } from 'react-bootstrap';
+import './Assignment5.css'; 
 
 export default function Assignment5( props ){
     // ============================= useState =============================
@@ -10,6 +12,10 @@ export default function Assignment5( props ){
     const [ mcomment, setMcomment ] = useState('');         // 간단한 소개
     const [ mpwd, setMpwd ] = useState('');                 // 비밀번호
     const [ movieList, setMovieList ] = useState([]);       // 추천영화 목록
+    const [ show, setShow ] = useState({show: false, mtitle : '', mno : 0});    // 모달
+    const [ discussionListByMno, setDiscussionListByMno ] = useState([]);       // 영화별 토론목록
+    const [ dcontent, setDcontent ] = useState('');         // 토론내용
+    const [ dpwd, setDpwd ] = useState('');                 // 토론 비밀번호
     // ============================= function =============================
     const postMovie = async ( ) => {
         // 1. 입력값 객체로 만들기
@@ -24,6 +30,7 @@ export default function Assignment5( props ){
             alert('등록 실패');
         } // if end
     } // func end
+
     const getMovies = async ( ) => {
         // 1. axios get
         const response = await axios.get( 'http://localhost:8080/movie/getMovies' );
@@ -32,10 +39,16 @@ export default function Assignment5( props ){
             setMovieList( response.data );
         } // if end
     } // func end
-    const deletePrompt = ( mno ) => {
+
+    const deletePrompt = ( mno, type ) => {
         const pwdInput = prompt('비밀번호를 입력하세요.');
-        if ( pwdInput != '' ) deleteMovie( mno, pwdInput );
+        if ( type == 'movie' ){
+            deleteMovie( mno, pwdInput );
+        } else if ( type == 'discussion' ){
+            deleteDicussion( mno, pwdInput );
+        } // if end
     } // func end
+
     const deleteMovie = async ( mno, mpwd ) => {
         // 1. 입력값 객체로 만들기
         const obj = { mno, mpwd };
@@ -49,6 +62,47 @@ export default function Assignment5( props ){
             alert('삭제 실패');
         } // if end
     } // func end
+
+    const postDiscussion = async ( mno ) => {
+        // 1. 입력값 객체로 만들기
+        const obj = { dcontent, dpwd, mno };
+        // 2. axios post
+        const response = await axios.post( 'http://localhost:8080/discussion/post', obj );
+        // 3. result
+        if ( response.status == 200 ){
+            alert('등록 성공');
+            getDiscussionByMno( mno );
+        } else {
+            alert('등록 실패');
+        } // if end
+    } // func end
+
+    const getDiscussionByMno = async ( mno ) => {
+        // 1. axios get
+        const response = await axios.get( `http://localhost:8080/discussion/getByMno?mno=${mno}` );
+        // 2. result
+        if ( response.data != null ){
+            setDiscussionListByMno( response.data );
+        } // if end
+        console.log( response.data );
+    } // func end
+
+    const deleteDicussion = async ( dno, dpwd ) => {
+        // 1. 입력값 객체로 만들기
+        const obj = { dno, dpwd };
+        // 2. axios put(delete)
+        const response = await axios.put( 'http://localhost:8080/discussion/delete', obj );
+        // 3. result
+        if ( response.status == 200 ){
+            alert('삭제 성공');
+            let newList = discussionListByMno.filter( (d) => {
+                return d.dno != dno;
+            })
+            setDiscussionListByMno( newList );
+        } else if ( response.status == 400 ) {
+            alert('삭제 실패');
+        } // if end
+    } // func end
     // ============================ useEffect =============================
     useEffect( ( ) => { getMovies() }, [ ] );
     // ============================== return ==============================
@@ -58,16 +112,16 @@ export default function Assignment5( props ){
             <div>
                 <h3> 영화 토론 플랫폼 </h3>
             </div>
-            <div>
-                <input type="text" placeholder='제목' value={mtitle} onChange={(e) => {setMtitle(e.target.value)}}/>
-                <input type="text" placeholder='감독' value={mdirector} onChange={(e) => {setMdirector(e.target.value)}}/>
-                <input type="text" placeholder='장르' value={mgenre} onChange={(e) => {setMgenre(e.target.value)}}/>
-                <input type="text" placeholder='간단한 소개' value={mcomment} onChange={(e) => {setMcomment(e.target.value)}}/>
-                <input type="password" placeholder='비밀번호' value={mpwd} onChange={(e) => {setMpwd(e.target.value)}}/>
-                <button onClick={postMovie}>등록</button>
+            <div class="inputGroup">
+                <Form.Control type="text" placeholder='제목' value={mtitle} onChange={(e) => {setMtitle(e.target.value)}}/>
+                <Form.Control type="text" placeholder='감독' value={mdirector} onChange={(e) => {setMdirector(e.target.value)}}/>
+                <Form.Control type="text" placeholder='장르' value={mgenre} onChange={(e) => {setMgenre(e.target.value)}}/>
+                <Form.Control type="text" placeholder='간단한 소개' value={mcomment} onChange={(e) => {setMcomment(e.target.value)}}/>
+                <Form.Control type="password" placeholder='비밀번호' value={mpwd} onChange={(e) => {setMpwd(e.target.value)}}/>
+                <Button variant="primary" onClick={postMovie}>등록</Button>
             </div>
             <div>
-                <table>
+                <Table striped bordered hover>
                     <thead>
                         <tr>
                             <th>번호</th>
@@ -89,14 +143,53 @@ export default function Assignment5( props ){
                                         <td>{movie.mdirector}</td>
                                         <td>{movie.mgenre}</td>
                                         <td>{movie.mcomment}</td>
-                                        <td><button onClick={() => {deletePrompt(movie.mno)}}>삭제</button></td>
+                                        <td>
+                                            <Button variant="primary" onClick={() => {setShow({show:true, mtitle : movie.mtitle, mno : movie.mno}); getDiscussionByMno(movie.mno)}}>
+                                                토론 페이지
+                                            </Button>
+                                            <Button variant="danger" onClick={() => {deletePrompt(movie.mno, 'movie')}}
+                                                >삭제
+                                            </Button>
+                                        </td>
                                     </tr>
+                                    <Modal show={show.show} onHide={() => setShow(false)} dialogClassName="modal-90w" aria-labelledby="example-custom-modal-styling-title">
+                                        <Modal.Header closeButton>
+                                        <Modal.Title id="example-custom-modal-styling-title">
+                                            {show.mtitle}의 토론페이지
+                                        </Modal.Title>
+                                        </Modal.Header>
+                                        <Modal.Body>
+                                            <div>
+                                                {
+                                                    discussionListByMno.map( (discussion) => {
+                                                        return(
+                                                            <>
+                                                            <div>
+                                                                <span>
+                                                                    {discussion.dcontent}
+                                                                </span>
+                                                                <Button variant="danger" onClick={() => {deletePrompt( discussion.dno, 'discussion')}}
+                                                                    >삭제
+                                                                </Button>
+                                                            </div>
+                                                            </>
+                                                        ) // return end
+                                                    }) // map end
+                                                }
+                                            </div>
+                                            <div class="inputGroup">
+                                                <Form.Control type="text" placeholder='토론내용' value={dcontent} onChange={(e) => {setDcontent(e.target.value)}}/>
+                                                <Form.Control type="password" placeholder='비밀번호' value={dpwd} onChange={(e) => {setDpwd(e.target.value)}}/>
+                                                <Button variant="primary" onClick={() => {postDiscussion(show.mno)}}>등록</Button>
+                                            </div>
+                                        </Modal.Body>
+                                    </Modal>
                                     </>
                                 ) // return end
                             }) // map end
                         }
                     </tbody>
-                </table>
+                </Table>
             </div>
         </div>
         </>

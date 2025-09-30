9a7e5a54baf2f5ce6e6a94172cb714194fe688b1
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-09-30 10:49:33 +0900

    feat : day89 수업 - Example3 : 메소드 레퍼런스

--- src/main/java/example/day11/Example3.java
+++ src/main/java/example/day11/Example3.java
@@ -15,12 +15,12 @@ public class Example3 {
         - 객체명::메소드명 vs 객체명.메소드명()
         */
 
-        // [1] 예제1
+        // [1] 예제1 : static 메소드 -> 객체 필요 X
         System.out.println(Integer.parseInt("123"));
         Function<String, Integer> function = Integer::parseInt;
         System.out.println(function.apply("123"));
 
-        // [2] 예제2
+        // [2] 예제2 : 메소드 -> 객체 필요 O
         System.out.println("=====");
         List<String> names = List.of("유재석", "강호동", "신동엽");
         for (String name : names){
@@ -30,5 +30,19 @@ public class Example3 {
         names.forEach(name -> System.out.println(name));
         System.out.println("=====");
         names.forEach(System.out::println);
+
+        // [3] 예제3
+        // 클래스명::new -> 생성자
+        names.forEach(name -> new Member(name));
+        names.forEach(Member::new);
+        // MyBatis SelectAll 자동화에서 사용가능(?)
+        List<Member> members = names.stream().map(Member::new).toList();
+        System.out.println("members = " + members);
     } // main end
+} // class end
+class Member{
+    String name;
+    Member(String name){
+        this.name = name;
+    } // func end
 } // class end

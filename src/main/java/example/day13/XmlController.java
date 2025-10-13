7e361f7074c67f33a4fe63d5af3910a73454d971
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-10-13 12:34:15 +0900

    feat : day91 수업

--- src/main/java/example/day13/XmlController.java
+++ src/main/java/example/day13/XmlController.java
@@ -15,7 +15,9 @@ public class XmlController {
     @PostMapping("")
     public ResponseEntity<?> save(@RequestBody StudentDto studentDto){
         // <?> : 제네릭 타입에 ?를 넣으면 모든 타입을 지칭한다.
+        System.out.println("studentDto = " + studentDto);
         xmlMapper.save(studentDto);
+        System.out.println("studentDto = " + studentDto);
         return ResponseEntity.ok(true);
     } // func end
 
@@ -24,5 +26,26 @@ public class XmlController {
     public ResponseEntity<?> getAll(){
         List<StudentDto> result = xmlMapper.getAll();
         return ResponseEntity.ok(result);
+    } // func end
+
+    // 3. 개별조회
+    @GetMapping("/bySno")
+    public ResponseEntity<?> getBySno(@RequestParam int sno){
+        StudentDto result = xmlMapper.getBySno(sno);
+        return ResponseEntity.ok(result);
+    } // func end
+
+    // 4. 개별삭제
+    @DeleteMapping("")
+    public ResponseEntity<?> deleteBySno(@RequestParam int sno){
+        xmlMapper.deleteBySno(sno);
+        return ResponseEntity.ok(true);
+    } // func end
+
+    // 5. 개별수정
+    @PutMapping("")
+    public ResponseEntity<?> updateBySno(@RequestBody StudentDto studentDto){
+        xmlMapper.updateBySno(studentDto);
+        return ResponseEntity.ok(true);
     } // func end
 } // class end
--- src/main/java/example/day13/XmlMapper.java
+++ src/main/java/example/day13/XmlMapper.java
@@ -1,18 +1,31 @@
 package example.day13;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 
 import java.util.List;
 
 @Mapper
 public interface XmlMapper {
     // MyBatis에서 SQL을 매핑하는 방법
-    // 방법1. 추상메소드에 @Insert("SQL") 추가, 간단한 CRUD
-    // 방법2. 추상메소드를 매핑하는 XML 파일에서 SQL 작성, 복잡한 SQL
+    // 방법1. 추상메소드에 @Insert("SQL") 추가, 주로 간단한 CRUD
+        // @Insert("INSERT INTO student(name, kor, math)\n VALUES (#{name}, #{kor}, #{math});")
+        // @Options(useGeneratedKeys = true, keyProperty = "sno")   // auto_increment 반환
+    // 방법2. 추상메소드를 매핑하는 XML 파일에서 SQL 작성, 주로 복잡한 SQL(동적 쿼리)
 
     // 1. 등록
     int save(StudentDto studentDto);
 
     // 2. 전체조회
     List<StudentDto> getAll();
+
+    // 3. 개별조회
+    StudentDto getBySno(int sno);
+
+    // 4. 개별삭제
+    int deleteBySno(int sno);
+
+    // 5. 개별수정
+    int updateBySno(StudentDto studentDto);
 } // interface end

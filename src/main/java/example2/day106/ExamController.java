657283bf7730f521fbc8b23ea6c00ecc5968f9ff
jeonghoonahn0510 <jeonghoonahn0510@gmail.com>
2025-11-03 12:16:26 +0900

    feat : day106 - JPA UD 추가

--- src/main/java/example2/day106/ExamController.java
+++ src/main/java/example2/day106/ExamController.java
@@ -12,21 +12,26 @@ public class ExamController {
 
     // 1. C
     // { "col1": 1, "col2": "유재석", "col3" : 3.14 }
-    @PostMapping("/save")
+    @PostMapping
     public ResponseEntity<?> save(@RequestBody ExamEntity examEntity){
         return ResponseEntity.ok(examService.save(examEntity));
     } // func end
 
     // 2. R
-    @GetMapping("/findall")
+    @GetMapping
     public ResponseEntity<?> findAll(){
         return ResponseEntity.ok(examService.findAll());
     } // func end
 
     // 3. U
-
+    @PutMapping
+    public ResponseEntity<?> update(@RequestBody ExamEntity examEntity){
+        return ResponseEntity.ok(examService.put2(examEntity));
+    } // func end
 
     // 4. D
-
-
+    @DeleteMapping
+    public ResponseEntity<?> delete(@RequestParam int col1){
+        return ResponseEntity.ok(examService.delete(col1));
+    } // func end
 } // class end
--- src/main/java/example2/day106/ExamService.java
+++ src/main/java/example2/day106/ExamService.java
@@ -1,12 +1,15 @@
 package example2.day106;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional  // SQL이 2개 이상이면, 트랜잭션 필수
 public class ExamService {
     // Mapper객체 -> Repository객체
     private final ExamRepository examRepository;
@@ -31,11 +34,37 @@ public class ExamService {
         return allEntities;
     } // func end
 
+    // 3-1. U(특정한 엔티티 수정) - 비권장(없으면 생성해버리기 때문에)
+    public ExamEntity put(ExamEntity examEntity){
+        // 1. Repository의 저장 메소드(.save())를 호출한다.
+        // .save(엔티티) : 만약에 지정한 엔티티에 PK가 없으면 생성, 있으면 수정
+        ExamEntity updatedEntity = examRepository.save(examEntity);
+        return updatedEntity;
+    } // func end
 
-    // 3. U
+    // 3-2. U(특정한 엔티티 수정) - 권장(트랜잭션 설정 필수)
+    // 엔티티를 setter하면, 자동으로 DB도 변경된다.
+    public ExamEntity put2(ExamEntity examEntity){
+        // 1. 수정할 엔티티를 조회한다.
+        Optional<ExamEntity> findEntity = examRepository.findById(examEntity.getCol1());
+        System.out.println("findEntity = " + findEntity);
+        // 2. Optional이란? 자주 발생하는 NullPointer 예외를 감싼 클래스
+        // -> Null 값에 대해 안전한 유효성 검사를 제공
+        if(findEntity.isPresent()){                     // .isPresent() : 존재 여부 확인
+            ExamEntity getEntity = findEntity.get();    // 존재하면, 엔티티 꺼내기
+            getEntity.setCol2(examEntity.getCol2());    // setter를 이용한 꺼낸 엔티티의 값 수정
+            getEntity.setCol3(examEntity.getCol3());    // setter를 이용한 꺼낸 엔티티의 값 수정
+            System.out.println("getEntity = " + getEntity);
+            return getEntity;
+        } // if end
+        return examEntity;
+    } // func end
 
-
-    // 4. D
-
-
+    // 4. D(삭제)
+    public boolean delete(int col1){
+        // 4-1. Repository의 삭제(.deleteById())를 호출한다.
+        // delete 자동처리
+        examRepository.deleteById(col1);
+        return true;
+    } // func end
 } // class end

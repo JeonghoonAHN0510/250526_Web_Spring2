3c8cef3ff5adc2d44a288dfb1444401e4d4b4c2f
jeonghoonahn0510 <jeonghoonahn0510@gmail.com>
2025-11-06 08:21:34 +0900

    feat : day109 - 백준 ing

--- src/main/java/backjoon/Main.java
+++ src/main/java/backjoon/Main.java
@@ -14,6 +14,29 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());   // 정점 N개
         int M = Integer.parseInt(st.nextToken());   // 간선 M개
         int R = Integer.parseInt(st.nextToken());   // 정점 R
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());   // 정점 u
+            int v = Integer.parseInt(st.nextToken());   // 정점 v
+            if (map.containsKey(u)){
+                map.get(u).add(v);
+            } else {
+                List<Integer> list = new ArrayList<>();
+                list.add(v);
+                map.put(u, list);
+            } // if end
+            if (map.containsKey(v)){
+                map.get(v).add(u);
+            } else {
+                List<Integer> list = new ArrayList<>();
+                list.add(u);
+                map.put(v, list);
+            } // if end
+        } // for end
+
+        boolean[] visited = new boolean[N];
+        visited[R] = true;
 
         bw.write(answer.toString().trim());
         bw.flush();

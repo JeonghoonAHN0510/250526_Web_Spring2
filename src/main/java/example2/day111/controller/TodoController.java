c7c542fd5f11aed66098e37db1d844cf55bdfade
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-11-10 11:56:08 +0900

    feat : day111 - repository 쿼리 메소드

--- src/main/java/example2/day111/controller/TodoController.java
+++ src/main/java/example2/day111/controller/TodoController.java
@@ -20,4 +20,18 @@ public class TodoController {
     public ResponseEntity<?> query1(@RequestParam String title){
         return ResponseEntity.ok(todoService.query1(title));
     } // func end
+
+    // 2. query2 실행해보기
+    @GetMapping("/query2")
+    public ResponseEntity<?> query2(@RequestParam String title,
+                                    @RequestParam String content){
+        return ResponseEntity.ok(todoService.query2(title, content));
+    } // func end
+
+    // 3. query3 실행해보기
+    @GetMapping("/query3")
+    public ResponseEntity<?> query3(@RequestParam(required = false) String keyword){
+        keyword = keyword == null ? "" : keyword;
+        return ResponseEntity.ok(todoService.query3(keyword));
+    } // func end
 } // class end
--- src/main/java/example2/day111/service/TodoService.java
+++ src/main/java/example2/day111/service/TodoService.java
@@ -33,4 +33,38 @@ public class TodoService {
                 .map(TodoEntity::toDto)
                 .collect(Collectors.toList());
     } // func end
+
+    // 2. query2 실행해보기
+    public List<TodoDto> query2(String title, String content){
+        // 내가 만든 쿼리메소드 사용해보기
+        List<TodoEntity> result1 = todoRepository.findByTitleAndContent(title, content);
+        System.out.println("result1 = " + result1);
+
+        // 네이티브 쿼리메소드 사용해보기
+        List<TodoEntity> result2 = todoRepository.query2(title, content);
+        System.out.println("result2 = " + result2);
+
+        // 반환하기
+        return result2
+                .stream()
+                .map(TodoEntity::toDto)
+                .collect(Collectors.toList());
+    } // func end
+
+    // 3. query3 실행해보기
+    public List<TodoDto> query3(String keyword){
+        // 내가 만든 쿼리메소드 사용해보기
+        List<TodoEntity> result1 = todoRepository.findByTitleContaining(keyword);
+        System.out.println("result1 = " + result1);
+
+        // 네이티브 쿼리메소드 사용해보기
+        List<TodoEntity> result2 = todoRepository.query3(keyword);
+        System.out.println("result2 = " + result2);
+
+        // 반환하기
+        return result2
+                .stream()
+                .map(TodoEntity::toDto)
+                .collect(Collectors.toList());
+    } // func end
 } // class end

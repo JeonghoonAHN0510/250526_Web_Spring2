3c6c277113c7a48693d67c8445bc123d63c59910
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-11-14 09:22:23 +0900

    feat : day114 - 백준 12865 평범한 배낭

--- src/main/java/backjoon/Main.java
+++ src/main/java/backjoon/Main.java
@@ -5,27 +5,36 @@ import java.util.*;
 
 public class Main {
     static StringBuilder answer = new StringBuilder();
+    static int N, K;
+    static int[][] array;
+    static int[] result;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        String str1 = br.readLine();
-        String str2 = br.readLine();
-        int length1 = str1.length();
-        int length2 = str2.length();
-        int[][] LCS = new int[length1 + 1][length2 + 1];
-        for (int i = 1; i <= length1; i++){
-            for (int j = 1; j <= length2; j++){
-                if (str1.charAt(i - 1) == str2.charAt(j - 1)){
-                    LCS[i][j] = LCS[i - 1][j - 1] + 1;
-                } else {
-                    LCS[i][j] = Math.max(LCS[i - 1][j], LCS[i][j - 1]);
-                } // if end
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());   // 물품의 수 N
+        K = Integer.parseInt(st.nextToken());   // 최대 무게 K
+        array = new int[N][2];
+        result = new int[K + 1];
+
+        for (int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            array[i][0] = Integer.parseInt(st.nextToken());
+            array[i][1] = Integer.parseInt(st.nextToken());
+        } // for end
+
+        for (int i = 0; i < N; i++) {
+            int weight = array[i][0];       // i번째 무게
+            int value = array[i][1];        // i번째 가치
+            for (int j = K; j >= weight; j--) {
+                // j번째 가치 VS i번쨰 가치 + j-i 무게의 가치
+                result[j] = Math.max(result[j], result[j - weight] + value);
             } // for end
         } // for end
 
-        answer.append(LCS[length1][length2]);
+        answer.append(result[K]);
 
         bw.write(answer.toString().trim());
         bw.flush();

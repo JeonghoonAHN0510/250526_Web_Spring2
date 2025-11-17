949f7b983eab745ae3859ca2cee43a60d7d6421c
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-11-17 09:31:49 +0900

    feat : day116 - 백준 12015 가장 긴 증가하는 부분 수열 2 푸는 중

--- src/main/java/backjoon/Main.java
+++ src/main/java/backjoon/Main.java
@@ -5,36 +5,26 @@ import java.util.*;
 
 public class Main {
     static StringBuilder answer = new StringBuilder();
-    static int N, K;
-    static int[][] array;
-    static int[] result;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
+        int N = Integer.parseInt(br.readLine());    // 수열의 크기 N
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());   // 물품의 수 N
-        K = Integer.parseInt(st.nextToken());   // 최대 무게 K
-        array = new int[N][2];
-        result = new int[K + 1];
-
+        int[] array = new int[N];
+        int[] result = new int[N];
         for (int i = 0; i < N; i++){
-            st = new StringTokenizer(br.readLine());
-            array[i][0] = Integer.parseInt(st.nextToken());
-            array[i][1] = Integer.parseInt(st.nextToken());
+            array[i] = Integer.parseInt(st.nextToken());
         } // for end
 
-        for (int i = 0; i < N; i++) {
-            int weight = array[i][0];       // i번째 무게
-            int value = array[i][1];        // i번째 가치
-            for (int j = K; j >= weight; j--) {
-                // j번째 가치 VS i번쨰 가치 + j-i 무게의 가치
-                result[j] = Math.max(result[j], result[j - weight] + value);
-            } // for end
+        result[0] = array[0];
+        int MaxLength = 1;
+        for (int i = 1; i < N; i++){
+
         } // for end
 
-        answer.append(result[K]);
+        answer.append(MaxLength);
 
         bw.write(answer.toString().trim());
         bw.flush();

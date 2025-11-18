048daf92eb55399b31778e8e91cff3d516e34c21
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-11-19 08:52:58 +0900

    feat : day118 - 백준 12015 가장 긴 증가하는 부분 수열 2

--- src/main/java/backjoon/Main.java
+++ src/main/java/backjoon/Main.java
@@ -21,7 +21,23 @@ public class Main {
         result[0] = array[0];
         int MaxLength = 1;
         for (int i = 1; i < N; i++){
-
+            int a = array[i];
+            if (result[MaxLength-1] < a){
+                MaxLength++;
+                result[MaxLength-1] = a;
+            } else {
+                int left = 0;
+                int right = MaxLength;
+                while (left < right){
+                    int mid = (left + right) / 2;
+                    if (result[mid] < a){
+                        left = mid + 1;
+                    } else {
+                        right = mid;
+                    } // if end
+                } // while end
+                result[left] = a;
+            } // if end
         } // for end
 
         answer.append(MaxLength);

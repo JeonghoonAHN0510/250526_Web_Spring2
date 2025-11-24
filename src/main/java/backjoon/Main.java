b99698d23e28efaed25d59d656f7eba61de6d150
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-11-25 08:58:36 +0900

    feat : day122 - 백준 2606 바이러스(BFS)

--- src/main/java/backjoon/Main.java
+++ src/main/java/backjoon/Main.java
@@ -7,56 +7,47 @@ public class Main {
     static StringBuilder answer = new StringBuilder();
     static Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
     static boolean[] visited;
-    static int[] visiting;
-    static int count = 1;
+    static int count = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());       // 정점의 수 N
-        int M = Integer.parseInt(st.nextToken());       // 간선의 수 M
-        int R = Integer.parseInt(st.nextToken());       // 시작 정점 R
-        visited = new boolean[N+1];
-        visiting = new int[N+1];
-
+        int N = Integer.parseInt(br.readLine());    // 컴퓨터 개수 N
+        int M = Integer.parseInt(br.readLine());    // 연결 개수 M
+        visited = new boolean[N + 1];
         for (int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
-            map.computeIfAbsent(u, f -> new PriorityQueue<>(Collections.reverseOrder())).add(v);
-            map.computeIfAbsent(v, f -> new PriorityQueue<>(Collections.reverseOrder())).add(u);
+            map.computeIfAbsent(x, k -> new PriorityQueue<>()).add(y);
+            map.computeIfAbsent(y, k -> new PriorityQueue<>()).add(x);
         } // for end
 
-        bfs(R);
+        bfs(1);
 
-        for (int i = 1; i <= N; i++){
-            answer.append(visiting[i]).append("\n");
-        } // for end
+        answer.append(count);
 
         bw.write(answer.toString().trim());
         bw.flush();
         bw.close();
     } // main end
-    public static void bfs(int R){
+    public static void bfs(int node){
         Queue<Integer> q = new LinkedList<>();
-        q.offer(R);
-        visited[R] = true;
+        q.offer(node);
+        visited[node] = true;
         while (!q.isEmpty()){
             int index = q.poll();
-            visiting[index] = count++;
-            if (map.containsKey(index)){
-                PriorityQueue<Integer> pq = map.get(index);
-                while (!pq.isEmpty()){
-                    int u = pq.poll();
-                    if (!visited[u]){
-                        visited[u] = true;
-                        q.offer(u);
-                    } // if end
-                } // while end
-            } // if end
+            PriorityQueue<Integer> pq = map.getOrDefault(index, new PriorityQueue<>());
+            while (!pq.isEmpty()){
+                int u = pq.poll();
+                if (!visited[u]){
+                    count++;
+                    visited[u] = true;
+                    q.offer(u);
+                } // if end
+            } // while end
         } // while end
     } // func end
 } // class end

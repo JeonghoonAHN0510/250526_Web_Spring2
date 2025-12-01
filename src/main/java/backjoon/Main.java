a1b83a69caa811e8593e45643b515dfd99fbcf5e
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-12-01 21:52:10 +0900

    feat : +Day03 - 백준 1012 유기농배추

--- src/main/java/backjoon/Main.java
+++ src/main/java/backjoon/Main.java
@@ -3,35 +3,44 @@ package backjoon;
 import java.awt.*;
 import java.io.*;
 import java.util.*;
+import java.util.List;
 
 public class Main {
     static StringBuilder answer = new StringBuilder();
-    static int[][] maze;
+    static int[][] array;
+    static List<Point> points;
     static boolean[][] visited;
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {1, -1, 0, 0};
-    static int N, M;
+    static int N, M, count;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());   // 세로 N
-        M = Integer.parseInt(st.nextToken());   // 가로 M
-        maze = new int[N][M];
-        visited = new boolean[N][M];
+        int T = Integer.parseInt(br.readLine());        // 테스트케이스 개수 T
 
-        for (int i = 0; i < N; i++){
-            String str = br.readLine();
-            for (int j = 0; j < M; j++){
-                maze[i][j] = str.charAt(j) - '0';
+        for (int t = 1; t <= T; t++){
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());       // 가로길이 N
+            M = Integer.parseInt(st.nextToken());       // 세로길이 M
+            array = new int[N+1][M+1];
+            visited = new boolean[N][M];
+            count = 0;
+            points = new ArrayList<>();
+            int K = Integer.parseInt(st.nextToken());   // 배추 위치 개수 K
+            for (int i = 0; i < K; i++){
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                array[x][y] = 1;
+                points.add(new Point(x, y));
             } // for end
+            for (Point point : points){
+                bfs(point.x, point.y);
+            } // for end
+            answer.append(count).append("\n");
         } // for end
-
-        bfs(0, 0);
-
-        answer.append(maze[N - 1][M - 1]);
 
         bw.write(answer.toString().trim());
         bw.flush();
@@ -40,25 +49,24 @@ public class Main {
     public static void bfs(int x, int y){
         Queue<Point> queue = new LinkedList<>();
         queue.add(new Point(x, y));
+        if (visited[x][y]) return;
         visited[x][y] = true;
         while (!queue.isEmpty()){
-            Point p = queue.poll();
-            for (int i = 0; i < 4; i++){
-                int nextX = p.x + dx[i];
-                int nextY = p.y + dy[i];
-                // 범위에 없으면, 다음 지점으로
+            Point point = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nextX = point.x + dx[i];
+                int nextY = point.y + dy[i];
+                // 범위 유효성 체크
                 if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) continue;
-                // 못가는 지점이면, 다음 지점으로
-                if (maze[nextX][nextY] == 0) continue;
-                // 왔던 지점으면, 다음 지점으로
+                // 배추 체크
+                if (array[nextX][nextY] == 0) continue;
+                // visited 체크
                 if (visited[nextX][nextY]) continue;
-                // 모든 조건 통과했으면, queue에 삽입
+                // queue에 삽입
                 queue.add(new Point(nextX, nextY));
-                // 방문 처리
                 visited[nextX][nextY] = true;
-                // 왔던 거리 누적
-                maze[nextX][nextY] = maze[p.x][p.y] + 1;
             } // for end
         } // while end
+        count++;
     } // func end
 } // class end

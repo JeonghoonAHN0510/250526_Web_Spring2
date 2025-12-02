ffbbbce563c7057839585981df35fc3005353597
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-12-02 21:55:31 +0900

    feat : +Day04 - 백준 1697 숨바꼭질(BFS)

--- src/main/java/backjoon/Main.java
+++ src/main/java/backjoon/Main.java
@@ -3,71 +3,55 @@ package backjoon;
 import java.awt.*;
 import java.io.*;
 import java.util.*;
-import java.util.List;
 
 public class Main {
     static StringBuilder answer = new StringBuilder();
-    static int[][] array;
-    static List<Point> points;
-    static Stack<Point> stack;
-    static boolean[][] visited;
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
-    static int N, M, count;
+    static boolean[] visited;
+    static int[] wNt = {-1, 1, 2};
+    static int N, K;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        int T = Integer.parseInt(br.readLine());        // 테스트케이스 개수 T
+        st = new StringTokenizer(br.readLine());
 
-        for (int t = 1; t <= T; t++){
-            st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());       // 가로길이 N
-            M = Integer.parseInt(st.nextToken());       // 세로길이 M
-            array = new int[N][M];
-            visited = new boolean[N][M];
-            count = 0;
-            points = new ArrayList<>();
-            stack = new Stack<>();
-            int K = Integer.parseInt(st.nextToken());   // 배추 위치 개수 K
-            for (int i = 0; i < K; i++){
-                st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-                array[x][y] = 1;
-                points.add(new Point(x, y));
-            } // for end
-            for (Point point : points){
-                dfs(point.x, point.y);
-            } // for end
-            answer.append(count).append("\n");
-        } // for end
+        N = Integer.parseInt(st.nextToken());   // 수빈 위치 N
+        K = Integer.parseInt(st.nextToken());   // 동생 위치 K
+        visited = new boolean[100001];
+
+        // 같은 위치면, BFS 실행 안하고 바로 0
+        if (N == K){
+            answer.append(0);
+        } else {
+            bfs(N);
+        } // if end
 
         bw.write(answer.toString().trim());
         bw.flush();
         bw.close();
     } // main end
-    public static void dfs(int x, int y){
-        stack.add(new Point(x, y));
-        if (visited[x][y]) return;
-        visited[x][y] = true;
-        while (!stack.isEmpty()){
-            Point point = stack.pop();
-            for (int i = 0; i < 4; i++) {
-                int nextX = point.x + dx[i];
-                int nextY = point.y + dy[i];
-                // 범위 유효성 체크
-                if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) continue;
-                // 배추 체크
-                if (array[nextX][nextY] == 0) continue;
-                // visited 체크
-                if (visited[nextX][nextY]) continue;
-                // queue에 삽입
-                stack.add(new Point(nextX, nextY));
-                visited[nextX][nextY] = true;
+    static void bfs(int N){
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(new Point(N, 0));
+        visited[N] = true;
+        while (!queue.isEmpty()){
+            Point point = queue.poll();
+            for (int i = 0; i < 3; i++){
+                int x = i == 2 ? point.x * wNt[i] : point.x + wNt[i];
+                // 다음 위치가 동생 위치라면, 횟수 저장하고 종료
+                if (x == K){
+                    answer.append(point.y + 1);
+                    return;
+                } // if end
+                // 범위 밖이라면, 다음 위치로
+                if (x < 0 || x >= 100001) continue;
+                // 왔던 위치라면, 다음 위치로
+                if (visited[x]) continue;
+                // 모든 조건 통과하면, 큐에 추가하여 반복 실행
+                queue.add(new Point(x, point.y + 1));
+                visited[x] = true;
             } // for end
         } // while end
-        count++;
     } // func end
 } // class end

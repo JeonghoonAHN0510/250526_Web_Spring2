ebe1f29e4b971c7cabddeaa426c546f485f14fd3
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-12-03 19:54:01 +0900

    feat : +Day05 - 백준 7562 나이트의 이동(BFS)

--- src/main/java/backjoon/Main.java
+++ src/main/java/backjoon/Main.java
@@ -6,51 +6,61 @@ import java.util.*;
 
 public class Main {
     static StringBuilder answer = new StringBuilder();
-    static boolean[] visited;
-    static int[] wNt = {-1, 1, 2};
-    static int N, K;
+    static int[][] chess;
+    static boolean[][] visited;
+    static int[] nx = {-2, -2, -1, -1, 1, 1, 2, 2};
+    static int[] ny = {1, -1, 2, -2, 2, -2, 1, -1};
+    static int I;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(br.readLine());        // 테스트 개수
 
-        N = Integer.parseInt(st.nextToken());   // 수빈 위치 N
-        K = Integer.parseInt(st.nextToken());   // 동생 위치 K
-        visited = new boolean[100001];
-
-        // 같은 위치면, BFS 실행 안하고 바로 0
-        if (N == K){
-            answer.append(0);
-        } else {
-            bfs(N);
-        } // if end
+        for (int i = 0; i < T; i++){
+            I = Integer.parseInt(br.readLine());    // 체스판 한 변의 길이
+            chess = new int[I][I];
+            visited = new boolean[I][I];
+            st = new StringTokenizer(br.readLine());    // 나이트 현재 위치
+            int knightX1 = Integer.parseInt(st.nextToken());
+            int knightY1 = Integer.parseInt(st.nextToken());
+            chess[knightX1][knightY1] = 0;
+            st = new StringTokenizer(br.readLine());    // 나이트 목적지
+            int knightX2 = Integer.parseInt(st.nextToken());
+            int knightY2 = Integer.parseInt(st.nextToken());
+            chess[knightX2][knightY2] = -1;
+            if (knightX1 == knightX2 && knightY1 == knightY2){
+                answer.append("0").append("\n");
+                continue;
+            } // if end
+            bfs(knightX1, knightY1);
+        } // for end
 
         bw.write(answer.toString().trim());
         bw.flush();
         bw.close();
     } // main end
-    static void bfs(int N){
+    static void bfs(int x, int y){
         Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(N, 0));
-        visited[N] = true;
+        queue.add(new Point(x, y));
+        visited[x][y] = true;
         while (!queue.isEmpty()){
             Point point = queue.poll();
-            for (int i = 0; i < 3; i++){
-                int x = i == 2 ? point.x * wNt[i] : point.x + wNt[i];
-                // 다음 위치가 동생 위치라면, 횟수 저장하고 종료
-                if (x == K){
-                    answer.append(point.y + 1);
+            for (int i = 0; i < 8; i++){
+                int nextX = point.x + nx[i];
+                int nextY = point.y + ny[i];
+                if (nextX < 0 || nextX >= I || nextY < 0 || nextY >= I) continue;
+                if (visited[nextX][nextY]) continue;
+
+                if (chess[nextX][nextY] == -1){
+                    answer.append(chess[point.x][point.y] + 1).append("\n");
                     return;
                 } // if end
-                // 범위 밖이라면, 다음 위치로
-                if (x < 0 || x >= 100001) continue;
-                // 왔던 위치라면, 다음 위치로
-                if (visited[x]) continue;
-                // 모든 조건 통과하면, 큐에 추가하여 반복 실행
-                queue.add(new Point(x, point.y + 1));
-                visited[x] = true;
+
+                chess[nextX][nextY] = chess[point.x][point.y] + 1;
+                queue.add(new Point(nextX, nextY));
+                visited[nextX][nextY] = true;
             } // for end
         } // while end
     } // func end

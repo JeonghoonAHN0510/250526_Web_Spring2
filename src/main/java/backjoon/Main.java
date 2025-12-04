408f2d85140f73fe38bb68ea15096c30e747e0af
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-12-04 10:11:22 +0900

    feat : +Day06 - 백준 7576 토마토(BFS)

--- src/main/java/backjoon/Main.java
+++ src/main/java/backjoon/Main.java
@@ -6,62 +6,70 @@ import java.util.*;
 
 public class Main {
     static StringBuilder answer = new StringBuilder();
-    static int[][] chess;
+    static int[][] tomato;
     static boolean[][] visited;
-    static int[] nx = {-2, -2, -1, -1, 1, 1, 2, 2};
-    static int[] ny = {1, -1, 2, -2, 2, -2, 1, -1};
-    static int I;
+    static Queue<Point> queue = new LinkedList<>();
+    static int[] nx = {0, 0, 1, -1};
+    static int[] ny = {1, -1, 0, 0};
+    static int N, M, zero, one;
+    static Point lastTomato = new Point(0, 0);
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        int T = Integer.parseInt(br.readLine());        // 테스트 개수
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());       // 가로 길이 N
+        M = Integer.parseInt(st.nextToken());       // 세로 길이 M
+        tomato = new int[M][N];
+        visited = new boolean[M][N];
+        zero = 0;
+        one = 0;
 
-        for (int i = 0; i < T; i++){
-            I = Integer.parseInt(br.readLine());    // 체스판 한 변의 길이
-            chess = new int[I][I];
-            visited = new boolean[I][I];
-            st = new StringTokenizer(br.readLine());    // 나이트 현재 위치
-            int knightX1 = Integer.parseInt(st.nextToken());
-            int knightY1 = Integer.parseInt(st.nextToken());
-            chess[knightX1][knightY1] = 0;
-            st = new StringTokenizer(br.readLine());    // 나이트 목적지
-            int knightX2 = Integer.parseInt(st.nextToken());
-            int knightY2 = Integer.parseInt(st.nextToken());
-            chess[knightX2][knightY2] = -1;
-            if (knightX1 == knightX2 && knightY1 == knightY2){
-                answer.append("0").append("\n");
-                continue;
-            } // if end
-            bfs(knightX1, knightY1);
+        for (int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++){
+                int a = Integer.parseInt(st.nextToken());
+                tomato[i][j] = a;
+                if (a == 1){
+                    queue.add(new Point(i, j));
+                    visited[i][j] = true;
+                    one++;
+                } else if (a == 0){
+                    zero++;
+                } else if (a == -1){
+                    one++;
+                } // if end
+            } // for end
         } // for end
+
+        if (one == N * M){
+            answer.append("0");
+        } else {
+            while (!queue.isEmpty()){
+                Point point = queue.poll();
+                for (int i = 0; i < 4; i++){
+                    int nextX = point.x + nx[i];
+                    int nextY = point.y + ny[i];
+                    if (nextX < 0 || nextX >= M || nextY < 0 || nextY >= N) continue;
+                    if (visited[nextX][nextY]) continue;
+                    if (tomato[nextX][nextY] == -1) continue;
+                    tomato[nextX][nextY] = tomato[point.x][point.y] + 1;
+                    queue.add(new Point(nextX, nextY));
+                    visited[nextX][nextY] = true;
+                    lastTomato = new Point(nextX, nextY);
+                    zero--;
+                } // for end
+            } // while end
+            if (zero == 0){
+                answer.append(tomato[lastTomato.x][lastTomato.y] - 1);
+            } else {
+                answer.append("-1");
+            } // if end
+        } // if end
 
         bw.write(answer.toString().trim());
         bw.flush();
         bw.close();
     } // main end
-    static void bfs(int x, int y){
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(x, y));
-        visited[x][y] = true;
-        while (!queue.isEmpty()){
-            Point point = queue.poll();
-            for (int i = 0; i < 8; i++){
-                int nextX = point.x + nx[i];
-                int nextY = point.y + ny[i];
-                if (nextX < 0 || nextX >= I || nextY < 0 || nextY >= I) continue;
-                if (visited[nextX][nextY]) continue;
-
-                if (chess[nextX][nextY] == -1){
-                    answer.append(chess[point.x][point.y] + 1).append("\n");
-                    return;
-                } // if end
-
-                chess[nextX][nextY] = chess[point.x][point.y] + 1;
-                queue.add(new Point(nextX, nextY));
-                visited[nextX][nextY] = true;
-            } // for end
-        } // while end
-    } // func end
 } // class end

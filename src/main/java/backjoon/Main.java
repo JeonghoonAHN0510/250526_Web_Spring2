3133a333fbd5be538c977acd9050b1ab4934d8f9
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-12-05 16:18:25 +0900

    feat : +Day07 - 백준 7569 토마토 3차원(BFS)

--- src/main/java/backjoon/Main.java
+++ src/main/java/backjoon/Main.java
@@ -4,15 +4,28 @@ import java.awt.*;
 import java.io.*;
 import java.util.*;
 
+class PointXYZ{
+    int x;
+    int y;
+    int z;
+
+    public PointXYZ(int x, int y, int z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    } // func end
+} // class end
+
 public class Main {
     static StringBuilder answer = new StringBuilder();
-    static int[][] tomato;
-    static boolean[][] visited;
-    static Queue<Point> queue = new LinkedList<>();
-    static int[] nx = {0, 0, 1, -1};
-    static int[] ny = {1, -1, 0, 0};
-    static int N, M, zero, one;
-    static Point lastTomato = new Point(0, 0);
+    static int[][][] tomato;
+    static boolean[][][] visited;
+    static Queue<PointXYZ> queue = new LinkedList<>();
+    static int[] nx = {0, 0, 1, -1, 0, 0};
+    static int[] ny = {1, -1, 0, 0, 0, 0};
+    static int[] nz = {0, 0, 0, 0, 1, -1};
+    static int N, M, H, zero, one;
+    static PointXYZ lastTomato = new PointXYZ(0, 0, 0);
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -21,48 +34,52 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());       // 가로 길이 N
         M = Integer.parseInt(st.nextToken());       // 세로 길이 M
-        tomato = new int[M][N];
-        visited = new boolean[M][N];
+        H = Integer.parseInt(st.nextToken());       // 상자의 수 H
+        tomato = new int[H][M][N];
+        visited = new boolean[H][M][N];
         zero = 0;
         one = 0;
 
-        for (int i = 0; i < M; i++){
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++){
-                int a = Integer.parseInt(st.nextToken());
-                tomato[i][j] = a;
-                if (a == 1){
-                    queue.add(new Point(i, j));
-                    visited[i][j] = true;
-                    one++;
-                } else if (a == 0){
-                    zero++;
-                } else if (a == -1){
-                    one++;
-                } // if end
+        for (int h = 0; h < H; h++){
+            for (int i = 0; i < M; i++){
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++){
+                    int a = Integer.parseInt(st.nextToken());
+                    tomato[h][i][j] = a;
+                    if (a == 1){
+                        queue.add(new PointXYZ(i, j, h));
+                        visited[h][i][j] = true;
+                        one++;
+                    } else if (a == 0){
+                        zero++;
+                    } else if (a == -1){
+                        one++;
+                    } // if end
+                } // for end
             } // for end
         } // for end
 
-        if (one == N * M){
+        if (one == N * M * H){
             answer.append("0");
         } else {
             while (!queue.isEmpty()){
-                Point point = queue.poll();
-                for (int i = 0; i < 4; i++){
+                PointXYZ point = queue.poll();
+                for (int i = 0; i < 6; i++){
                     int nextX = point.x + nx[i];
                     int nextY = point.y + ny[i];
-                    if (nextX < 0 || nextX >= M || nextY < 0 || nextY >= N) continue;
-                    if (visited[nextX][nextY]) continue;
-                    if (tomato[nextX][nextY] == -1) continue;
-                    tomato[nextX][nextY] = tomato[point.x][point.y] + 1;
-                    queue.add(new Point(nextX, nextY));
-                    visited[nextX][nextY] = true;
-                    lastTomato = new Point(nextX, nextY);
+                    int nextZ = point.z + nz[i];
+                    if (nextX < 0 || nextX >= M || nextY < 0 || nextY >= N || nextZ < 0 || nextZ >= H) continue;
+                    if (visited[nextZ][nextX][nextY]) continue;
+                    if (tomato[nextZ][nextX][nextY] == -1) continue;
+                    tomato[nextZ][nextX][nextY] = tomato[point.z][point.x][point.y] + 1;
+                    queue.add(new PointXYZ(nextX, nextY, nextZ));
+                    visited[nextZ][nextX][nextY] = true;
+                    lastTomato = new PointXYZ(nextX, nextY, nextZ);
                     zero--;
                 } // for end
             } // while end
             if (zero == 0){
-                answer.append(tomato[lastTomato.x][lastTomato.y] - 1);
+                answer.append(tomato[lastTomato.z][lastTomato.x][lastTomato.y] - 1);
             } else {
                 answer.append("-1");
             } // if end
